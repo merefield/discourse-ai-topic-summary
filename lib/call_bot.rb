910151f5b3a44ae0e707e98ba5c9e13a015aabee
merefield <merefield@gmail.com>
2023-02-21 22:11:25 +0000

    Update namespace

--- lib/call_bot.rb
+++ lib/call_bot.rb
@@ -1,8 +1,13 @@
 require "openai"
 
-class ::AITopicSummary::CallBot
+class ::AiTopicSummary::CallBot
   # see https://github.com/alexrudall/ruby-openai
   def self.get_response(full_raw)
+
+    # OpenAI.configure do |config|
+    #   config.request_timeout = 25
+    # end
+
     client = OpenAI::Client.new(access_token: SiteSetting.ai_topic_summary_open_ai_token)
 
     response = client.completions(
