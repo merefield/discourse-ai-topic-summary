3abfd0d567ba0d25cf6d1074c02d40529de0ba78
merefield <merefield@gmail.com>
2023-02-23 09:16:36 +0000

    IMPROVE: raise & log useful error when one is returned from OpenAI

--- lib/call_bot.rb
+++ lib/call_bot.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
 require "openai"
+
+class OpenAICallError < StandardError
+  attr_reader :foo
+
+  def initialize(foo)
+   super
+   @foo = foo
+  end
+end
 
 class ::AiTopicSummary::CallBot
   # see https://github.com/alexrudall/ruby-openai
   def self.get_response(full_raw)
 
+    # TODO add this in when support added via PR after "ruby-openai", '3.3.0'
     # OpenAI.configure do |config|
     #   config.request_timeout = 25
     # end
@@ -17,8 +28,12 @@ class ::AiTopicSummary::CallBot
           max_tokens: SiteSetting.ai_topic_summary_max_response_tokens
       })
 
+    if response.parsed_response["error"]
+      raise StandardError, response.parsed_response["error"]["message"]
+    end
+
     final_text = response["choices"][0]["text"]
 
-    return final_text
+    final_text
   end
 end
