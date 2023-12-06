090baaaf04bd4be91d90b8133b2108266ad2ef39
merefield <merefield@gmail.com>
2023-12-06 12:12:16 +0000

    up open ai gem

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 # name: discourse-ai-topic-summary
 # about: Uses a remote (OpenAI) AI language model to prepare and post a summary of a Topic
-# version: 0.0.14
+# version: 0.0.15
 # authors: Robert Barrow
 # contact_emails: merefield@gmail.com
 # url: https://github.com/merefield/discourse-ai-topic-summary
 
 gem 'multipart-post', '2.3.0', { require: false }
 gem 'faraday-multipart', '1.0.4', { require: false }
-gem "ruby-openai", '5.1.0', { require: false }
+gem 'event_stream_parser', '1.0.0', { require: false }
+gem "ruby-openai", '6.3.1', { require: false }
 
 register_asset 'stylesheets/common/ai_topic_summary.scss'
 
