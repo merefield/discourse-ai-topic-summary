1fdef8ed743d68a817f823b24dcdc873d39c1682
merefield <merefield@gmail.com>
2024-05-17 12:21:15 +0100

    IMPROVE: logged error

--- app/jobs/regular/ai_topic_summary_summarise_topic.rb
+++ app/jobs/regular/ai_topic_summary_summarise_topic.rb
@@ -6,8 +6,15 @@ class ::Jobs::AiTopicSummarySummariseTopic < Jobs::Base
   sidekiq_options retry: 5
 
   def execute(opts)
-    topic_id = opts[:topic_id]
+    begin
+      topic_id = opts[:topic_id]
 
-    ::AiTopicSummary::Summarise.retrieve_and_post_summary(topic_id)
+      ::AiTopicSummary::Summarise.retrieve_and_post_summary(topic_id)
+
+    rescue => e
+      status = e.response[:status]
+      message = e.response[:body]["error"]["message"]
+      Rails.logger.error("AI Topic Summary: Summarise Job: There was a problem, but will retry til limit: status: #{status}, message: #{message}")
+    end
   end
 end
